12b1ed2e27b8cefda816c1334bef8f4c84a8b7a0
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2022-02-24 16:14:21 +0100

    Write index.txt and changes.csv

--- cmd/csaf_aggregator/indices.go
+++ cmd/csaf_aggregator/indices.go
@@ -9,16 +9,76 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
 	"log"
+	"os"
 	"path/filepath"
+	"sort"
+	"strconv"
+	"time"
 )
 
 func (w *worker) writeCSV(fname string, summaries []summary) error {
 
-	log.Printf("CSV: %s\n", fname)
-	// TODO: Implement me!
+	// Do not sort in-place.
+	ss := make([]summary, len(summaries))
+	copy(ss, summaries)
 
-	return nil
+	sort.SliceStable(ss, func(i, j int) bool {
+		return ss[i].summary.CurrentReleaseDate.After(
+			ss[j].summary.CurrentReleaseDate)
+	})
+
+	f, err := os.Create(fname)
+	if err != nil {
+		return err
+	}
+	out := csv.NewWriter(f)
+
+	record := make([]string, 2)
+
+	for i := range ss {
+		s := &ss[i]
+		record[0] =
+			s.summary.CurrentReleaseDate.Format(time.RFC3339)
+		record[1] =
+			strconv.Itoa(s.summary.InitialReleaseDate.Year()) + "/" + s.filename
+		if err := out.Write(record); err != nil {
+			f.Close()
+			return err
+		}
+	}
+	out.Flush()
+	err1 := out.Error()
+	err2 := f.Close()
+	if err1 != nil {
+		return err1
+	}
+	return err2
+}
+
+func (w *worker) writeIndex(fname string, summaries []summary) error {
+
+	f, err := os.Create(fname)
+	if err != nil {
+		return err
+	}
+	out := bufio.NewWriter(f)
+	for i := range summaries {
+		s := &summaries[i]
+		fmt.Fprintf(
+			out, "%d/%s\n",
+			s.summary.InitialReleaseDate.Year(),
+			s.filename)
+	}
+	err1 := out.Flush()
+	err2 := f.Close()
+	if err1 != nil {
+		return err1
+	}
+	return err2
 }
 
 func (w *worker) writeIndices() error {
@@ -27,11 +87,14 @@ func (w *worker) writeIndices() error {
 		return nil
 	}
 
-	log.Printf("%s\n", w.dir)
-
 	for label, summaries := range w.summaries {
 		log.Printf("%s: %d\n", label, len(summaries))
-		if err := w.writeCSV(filepath.Join(w.dir, label, "changes.csv"), summaries); err != nil {
+		csvFile := filepath.Join(w.dir, label, "changes.csv")
+		if err := w.writeCSV(csvFile, summaries); err != nil {
+			return err
+		}
+		indexFile := filepath.Join(w.dir, label, "index.txt")
+		if err := w.writeIndex(indexFile, summaries); err != nil {
 			return err
 		}
 	}
