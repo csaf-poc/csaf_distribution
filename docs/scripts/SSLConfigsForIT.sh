a529fa7036c3f7a65c11b4d67b57c5678a2ea2a9
Fadi Abbud <fadi.abbud@intevation.de>
2022-03-28 17:04:39 +0200

    Change path to ssl certificates (SSLConfigsForIT.sh)

--- docs/scripts/SSLConfigsForIT.sh
+++ docs/scripts/SSLConfigsForIT.sh
@@ -30,8 +30,8 @@ echo '
         listen 443 ssl default_server; # ipv4
         listen [::]:443 ssl http2 default_server;  # ipv6
 
-       '${SSL_CERTIFICATE}' # e.g. ssl_certificate devca1/bundle.crt
-       '${SSL_CERTIFICATE_KEY}' # e.g. ssl_certificate_key devca1/testserver-key.pem;
+       '${SSL_CERTIFICATE}' # e.g. ssl_certificate /etc/ssl/csaf/bundle.crt
+       '${SSL_CERTIFICATE_KEY}' # e.g. ssl_certificate_key /etc/ssl/csaf/testserver-key.pem;
 
         ssl_protocols TLSv1.2 TLSv1.3;
 
