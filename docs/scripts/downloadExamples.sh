ffa830ac20553f293c100fadef96af0c2a4dc8eb
Bernhard Reiter <bernhard@intevation.de>
2022-04-01 16:21:43 +0200

    Improve downloadExamples.sh
     * Add comment with security advise.

--- docs/scripts/downloadExamples.sh
+++ docs/scripts/downloadExamples.sh
@@ -18,6 +18,9 @@ curl --silent --show-error -H 'Accept: application/vnd.github.v3.raw' \
 mkdir csaf_examples
 cd csaf_examples
 
+# careful with automation here, in theory lines in
+# ../csaf_examples_pathnames.txt could contain anything, because the json
+# objects are controlled by the file names in that directory and the gitup api
 cat ../csaf_examples_pathnames.txt | \
  xargs -I {} \
   curl --silent --show-error -H 'Accept: application/vnd.github.v3.raw' \
