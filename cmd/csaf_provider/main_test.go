85447c20b7179144b60002682cf8dcaca072f955
Bernhard Reiter <bernhard@intevation.de>
2022-04-13 14:58:49 +0200

    Extend itest coverage prodiver example
     * Add how to create a coverage file per cgi call and merge them in the
       description which is in the code comments.

--- cmd/csaf_provider/main_test.go
+++ cmd/csaf_provider/main_test.go
@@ -15,11 +15,17 @@ import (
 //   pushd /usr/lib/cgi-bin
 //   mv csaf_provider.go csaf_provider2.go
 //   echo '#!/bin/bash
-//   exec /usr/lib/cgi-bin/csaf_provider.debug -test.coverprofile=/tmp/csaf_provider-itest.cov -- "$@"
+//   exec /usr/lib/cgi-bin/csaf_provider.debug -test.coverprofile=/tmp/csaf_provider-itest-${EPOCHREALTIME}.cov -- "$@"
 //   ' >csaf_provider.go
 //   chmod a+x csaf_provider.go
 //
 // then do a cgi-bin action on the provider like using the uploader
+//
+// If you want to merge several runs, try
+//   go install github.com/wadey/gocovmerge@b5bfa59ec0adc420475f97f89b58045c721d761c
+//   ~/go/bin/gocovmerge /tmp/csaf_provider-ites*.cov >csaf_provider-itest-merged.cov
+//   cd ~/csaf_distribution
+//   go tool cover -func=~/csaf_provider-itest-merged.cov
 
 func TestMain(t *testing.T) {
 	var endOfTestParams int
