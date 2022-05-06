d979bb856b962cd874b411da88e720a6709402da
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2022-05-06 10:44:37 +0200

    Use changes.csv for scanning for interim files

--- cmd/csaf_aggregator/interim.go
+++ cmd/csaf_aggregator/interim.go
@@ -9,29 +9,49 @@
 package main
 
 import (
-	"bufio"
+	"encoding/csv"
 	"encoding/json"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
-	"strconv"
 	"time"
 
 	"github.com/csaf-poc/csaf_distribution/util"
 )
 
-func loadIndexFromReader(r io.Reader, accept func(string) bool) ([]string, error) {
-	var files []string
-	scanner := bufio.NewScanner(r)
+// loadChangesFromReader scans a changes.csv file for matching
+// iterim advisories. changes.txt are sorted with youngest
+// first, so we can stop scanning if entries get too old.
+func loadChangesFromReader(
+	r io.Reader,
+	accept func(time.Time, string) (bool, bool),
+) ([]string, error) {
 
-	for scanner.Scan() {
-		file := scanner.Text()
-		if accept(file) {
-			files = append(files, file)
+	changes := csv.NewReader(r)
+	changes.FieldsPerRecord = 2
+
+	var files []string
+
+	for {
+		record, err := changes.Read()
+		if err == io.EOF {
+			break
 		}
-	}
-	if err := scanner.Err(); err != nil {
-		return nil, err
+		if err != nil {
+			return nil, err
+		}
+		t, err := time.Parse(time.RFC3339, record[0])
+		if err != nil {
+			return nil, err
+		}
+		take, cont := accept(t, record[1])
+		if take {
+			files = append(files, record[1])
+		}
+		if !cont {
+			break
+		}
 	}
 
 	return files, nil
@@ -43,45 +63,42 @@ func scanForInterimFiles(base string, years int) ([]string, error) {
 		years = 10_000
 	}
 
-	from := time.Now().Year() - years
+	from := time.Now().AddDate(-years, 0, 0)
 
 	pe := util.NewPathEval()
 
-	accept := func(fname string) bool {
-		dname := filepath.Dir(fname)
-		year, err := strconv.Atoi(dname)
-		if err != nil {
-			return false
-		}
-
-		if year < from {
-			return false
+	accept := func(t time.Time, fname string) (bool, bool) {
+		if t.Before(from) {
+			return false, false
 		}
 
 		fn := filepath.Join(base, fname)
 		f, err := os.Open(fn)
 		if err != nil {
-			return false
+			log.Printf("error: %v\n", err)
+			return false, true
 		}
 		defer f.Close()
 
 		var doc interface{}
 		if err := json.NewDecoder(f).Decode(&doc); err != nil {
-			return false
+			log.Printf("error: %v\n", err)
+			return false, true
 		}
 
 		const interimExpr = `$.document.status"`
 
 		var status string
-		return pe.Extract(interimExpr, util.StringMatcher(&status), doc) == nil &&
+		matches := pe.Extract(interimExpr, util.StringMatcher(&status), doc) == nil &&
 			status == "interim"
+		return matches, true
 	}
 
-	indexF, err := os.Open(filepath.Join(base, "index.txt"))
+	changesF, err := os.Open(filepath.Join(base, "changes.csv"))
 	if err != nil {
 		return nil, err
 	}
-	defer indexF.Close()
+	defer changesF.Close()
 
-	return loadIndexFromReader(indexF, accept)
+	return loadChangesFromReader(changesF, accept)
 }
