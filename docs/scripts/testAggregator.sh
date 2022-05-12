02760ff004479be9e76a8ce1793528e676910985
Fadi Abbud <fadi.abbud@intevation.de>
2022-05-12 10:09:57 +0200

    Change group/mode of the directory for aggregator

--- docs/scripts/testAggregator.sh
+++ docs/scripts/testAggregator.sh
@@ -8,5 +8,9 @@
 # SPDX-FileCopyrightText: 2022 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
 # Software-Engineering: 2022 Intevation GmbH <https://intevation.de>
 
+# The same directory name used in the config-example file.
+sudo chgrp -R www-data  /var/csaf_aggregator
+sudo chmod -R g+w  /var/csaf_aggregator
+
 cd ~/csaf_distribution/
 sudo ./bin-linux-amd64/csaf_aggregator -c docs/examples/aggregator.toml
