2561c8555fca76f7b537a64575188184abf07b39
Fadi Abbud <fadi.abbud@intevation.de>
2022-05-12 09:34:01 +0200

    Include testing aggregator in itests.

---
+++ docs/scripts/testAggregator.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This file is Free Software under the MIT License
+# without warranty, see README.md and LICENSES/MIT.txt for details.
+#
+# SPDX-License-Identifier: MIT
+#
+# SPDX-FileCopyrightText: 2022 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+# Software-Engineering: 2022 Intevation GmbH <https://intevation.de>
+
+cd ~/csaf_distribution/
+./bin-linux-amd64/csaf_aggregator -c docs/examples/aggregator.toml
