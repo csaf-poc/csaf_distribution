6ec2131f5d1c2a874d66145d350c54298695d2a8
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2022-06-09 17:17:43 +0200

    Fix datetime in checker reports
    * Change how the datetime is generated for the report.
    
    Co-authored-by: Bernhard Reiter <bernhard@intevation.de>

--- cmd/csaf_checker/processor.go
+++ cmd/csaf_checker/processor.go
@@ -192,7 +192,10 @@ func (p *processor) clean() {
 // It returns a pointer to the report and nil, otherwise an error.
 func (p *processor) run(reporters []reporter, domains []string) (*Report, error) {
 
-	var report Report
+	report := Report{
+		Date:    ReportTime{Time: time.Now().UTC()},
+		Version: util.SemVersion,
+	}
 
 	for _, d := range domains {
 		if err := p.checkDomain(d); err != nil {
@@ -205,9 +208,6 @@ func (p *processor) run(reporters []reporter, domains []string) (*Report, error)
 		for _, r := range reporters {
 			r.report(p, domain)
 		}
-		report.Version = util.SemVersion
-		now := time.Now()
-		report.Date = now.Format("2006-01-02T15:04:05Z")
 		report.Domains = append(report.Domains, domain)
 		p.clean()
 	}
