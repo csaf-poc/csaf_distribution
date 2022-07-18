383afab148bf28730b977a2dab83ea06a9caed57
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2022-07-18 13:04:12 +0200

    Added Go lisense

--- util/joinpath.go
+++ util/joinpath.go
@@ -1,10 +1,13 @@
 // This file is Free Software under the MIT License
 // without warranty, see README.md and LICENSES/MIT.txt for details.
 //
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BSD
 //
-// SPDX-FileCopyrightText: 2022 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+// SPDX-FileCopyrightText: 2009 The Go Authors
 // Software-Engineering: 2022 Intevation GmbH <https://intevation.de>
+//
+// The code of this file was extracted and adjusted from
+// https://cs.opensource.google/go/go/+/refs/tags/go1.19rc2:src/net/url/url.go
 
 //go:build !go1.19
 
