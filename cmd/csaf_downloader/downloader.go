760c4f288473e1a8516931513adff4ade071d8fa
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2023-02-01 12:59:19 +0100

    Fix small typo in reporting wromg content type

--- cmd/csaf_downloader/downloader.go
+++ cmd/csaf_downloader/downloader.go
@@ -284,7 +284,7 @@ func (d *downloader) downloadFiles(label csaf.TLPLabel, files []csaf.AdvisoryFil
 		// Warn if we do not get JSON.
 		if ct := resp.Header.Get("Content-Type"); ct != "application/json" {
 			log.Printf(
-				"WARN: The content type of %s should be 'application/json but is '%s'\n",
+				"WARN: The content type of %s should be 'application/json' but is '%s'\n",
 				file.URL(), ct)
 		}
 
