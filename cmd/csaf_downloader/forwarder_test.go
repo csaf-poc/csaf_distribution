3ab78f87d40fd03153db171d48d84384da30dbf3
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2023-09-28 00:07:55 +0200

    Add unit test for stats logging in forwarder.

--- cmd/csaf_downloader/forwarder_test.go
+++ cmd/csaf_downloader/forwarder_test.go
@@ -8,7 +8,13 @@
 
 package main
 
-import "testing"
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"log/slog"
+	"testing"
+)
 
 func TestValidationStatusUpdate(t *testing.T) {
 	sv := validValidationStatus
@@ -22,5 +28,57 @@ func TestValidationStatusUpdate(t *testing.T) {
 	sv.update(notValidatedValidationStatus)
 	if sv != notValidatedValidationStatus {
 		t.Fatalf("got %q expected %q", sv, notValidatedValidationStatus)
+	}
+}
+
+func TestForwarderLogStats(t *testing.T) {
+	orig := slog.Default()
+	defer slog.SetDefault(orig)
+
+	var buf bytes.Buffer
+	h := slog.NewJSONHandler(&buf, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})
+	lg := slog.New(h)
+	slog.SetDefault(lg)
+
+	cfg := &config{}
+	fw := newForwarder(cfg)
+	fw.failed = 11
+	fw.succeeded = 13
+
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		fw.run()
+	}()
+	fw.log()
+	fw.close()
+	<-done
+
+	type fwStats struct {
+		Msg       string `json:"msg"`
+		Succeeded int    `json:"succeeded"`
+		Failed    int    `json:"failed"`
+	}
+	sc := bufio.NewScanner(bytes.NewReader(buf.Bytes()))
+	found := false
+	for sc.Scan() {
+		var fws fwStats
+		if err := json.Unmarshal(sc.Bytes(), &fws); err != nil {
+			t.Fatalf("JSON parsing log failed: %v", err)
+		}
+		if fws.Msg == "Forward statistics" &&
+			fws.Failed == 11 &&
+			fws.Succeeded == 13 {
+			found = true
+			break
+		}
+	}
+	if err := sc.Err(); err != nil {
+		t.Fatalf("scanning log failed: %v", err)
+	}
+	if !found {
+		t.Fatal("Cannot find forward statistics in log")
 	}
 }
