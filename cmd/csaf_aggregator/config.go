3923dc7044df48f652288c97a374163a7f84dd25
Bernhard Reiter <bernhard@intevation.de>
2023-10-17 11:33:03 +0200

    fix: improve logging for downloader and aggregator
     * use full name for printing out the used logfile for the downloader.
     * for debug or verbose, log the timeintervall that will be used
       for downloader and aggregator. (The checker has this as part
       of its output already.)

--- cmd/csaf_aggregator/config.go
+++ cmd/csaf_aggregator/config.go
@@ -12,6 +12,7 @@ import (
 	"crypto/tls"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"runtime"
@@ -166,14 +167,21 @@ func (c *config) tooOldForInterims() func(time.Time) bool {
 // is in the accepted download interval of the provider or
 // the global config.
 func (p *provider) ageAccept(c *config) func(time.Time) bool {
+	var r *models.TimeRange
 	switch {
 	case p.Range != nil:
-		return p.Range.Contains
+		r = p.Range
 	case c.Range != nil:
-		return c.Range.Contains
+		r = c.Range
 	default:
 		return nil
 	}
+
+	if c.Verbose {
+		s, _ := r.MarshalJSON()
+		log.Printf("Setting up filter to accept docs within TimeRange %s", s)
+	}
+	return r.Contains
 }
 
 // ignoreFile returns true if the given URL should not be downloaded.
